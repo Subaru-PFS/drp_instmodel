eacd0a077eb63c0f7fc1811612770760ee8f0fc5
Craig Loomis <cloomis@astro.princeton.edu>
2012-09-21 11:00:51 -0400

    pull out config routine

---
+++ python/pfs_instmodel/configFile.py
@@ -0,0 +1,11 @@
+
+def readfile(self, configFile, outDict=None):
+    """ execute a python file, and return the final environment. """
+    
+    gdict = {}
+    ldict = {}
+        
+    execfile(configFile,  gdict, ldict)
+    if outDict:
+        outDict.update(ldict)
+    return ldict
