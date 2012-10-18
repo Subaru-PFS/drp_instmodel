fix normalization by having spot fux sum to 1.0

python/pfs_instmodel/jegSpots.py
@@ -1,6 +1,7 @@
 import glob
 import os
 import re
+import sys
 
 import numpy
 import scipy.signal
@@ -125,6 +126,9 @@ def readSpotDir(path, doConvolve=True):
             spot = convolveWithFiber(data[i,:,:], fiberImage)
         else:
             spot = data[i,:,:]
+        # Require total flux to be 1.0
+        spot /= spot.sum()
+        
         spots.append((fiberIdx, wavelength, xc, yc, spot))
         print("spot  %d (%d, %0.2f) sum=%f" % (i, fiberIdx, wavelength, sum))
         if fiberIdx != 0:
@@ -139,9 +143,6 @@ def readSpotDir(path, doConvolve=True):
 
     tarr = numpy.array(allSpots, dtype=spotDtype)
 
-    # Normalize
-    tarr['spot'] /= tarr['spot'].max()
-    
     # Now clean up... later steps expect to have wavelengths in order
     sortfrom = numpy.argsort(tarr['wavelength'][:nlam])
     arr = numpy.zeros(shape=tarr.shape, dtype=tarr.dtype)
@@ -195,5 +196,6 @@ def main(band):
     print "splining..."
     splinedPsf.constructSplinesFromSpots(band, spotType='jeg')
 
+    
 if __name__ == "__main__":
     main(sys.argv[1])

python/pfs_instmodel/splinedPsf.py
@@ -214,7 +214,7 @@ class SplinedPsf(psf.Psf):
 
         # Evaluate at highest resolution
         pixelScale = self.spotScale
-        everyNthPsf *= self.detector.config['pixelScale'] / self.spotScale
+        everyNthPsf *= int(self.detector.config['pixelScale'] / self.spotScale)
         
         if waveRange == None:
             waveRange = self.wave.min(), self.wave.max()
@@ -294,20 +294,17 @@ class SplinedPsf(psf.Psf):
             intx = round(xPixOffset)
 
             if i % 1000 in range(2):
-                print("%5d %6.1f xc: %3.4f yc: %3.4f %3.10f %d %d %3.10f " % (i, specWave, xc, yc, yPixOffset, lasty, inty, fracy))
+                print("%5d %6.1f xc: %3.4f yc: %3.4f %3.10f %d %d %3.10f %0.1f" % (i, specWave, xc, yc, yPixOffset, lasty, inty, fracy, specFlux))
             lasty = inty
             
             # Assume we are well enough oversampled to ignore fractional pixel shifts.
             spot = specFlux * rawPsf
-            # XXXX Hack to bring flux below saturation
-            spot /= 4
             
             self.placeSubimage(fiberImage, spot, intx, inty)
                         
             # bin psf to ccd pixels, shift by fractional pixel only.
             #psf = self.scalePsf(rawPsf, -fracx, -fracy, doDetails=doDetails)
             #self.placeSubimage(outImg, spot, intx, inty)
-
 
         # transfer flux from oversampled fiber image to final resolution output image
         resampledFiber = self.addOversampledImage(fiberImage, outImg, outImgOffset, outImgSpotPixelScale)
@@ -330,10 +327,9 @@ class SplinedPsf(psf.Psf):
         
         shape = a.shape
         lenShape = len(shape)
-        factor = np.asarray(shape)/np.asarray(args)
         evList = ['a.reshape('] + \
                  ['args[%d],factor[%d],'%(i,i) for i in range(lenShape)] + \
-                 [')'] + ['.mean(%d)'%(i+1) for i in range(lenShape)]
+                 [')'] + ['.sum(%d)'%(i+1) for i in range(lenShape)]
 
         return eval(''.join(evList))
     
@@ -470,7 +466,7 @@ def constructSplinesFromSpots(band, spotType='zemax'):
     outputDict['xc'] = s['spot_xc']
     outputDict['yc'] = s['spot_yc']
 
-    spots = s['spot'].astype('float64')
+    spots = s['spot'].astype('float32')
     outputDict['spots'] = spots
 
     imshape = spots.shape[1:]