cfc6403e5bb97ac1eb2081935f9b0daf9fd14c8f
Craig Loomis <cloomis@astro.princeton.edu>
2012-11-30 16:36:31 -0500

    . add optional pixel response . add call to scipy spline interpolator. . fix bypassing lanczos windowing by setting n=0; must set padding to get wide sinc kernel. . have gatherPoo shift by all available X subpixels

--- python/pfs_instmodel/spotgames.py
+++ python/pfs_instmodel/spotgames.py
@@ -73,11 +73,21 @@ def shiftSpot2d(spot, dx, dy, kernels=None, kargs=None):
         xkernel = make1dKernel(offset=dx, **kargs)[1]
         ykernel = make1dKernel(offset=dy, **kargs)[1]
         kernel = numpy.outer(ykernel, xkernel)
-        kernels = (kernel, xkernel, ykernel)
+        kernels = (xkernel, ykernel, kernel)
 
     sspot = scipy.signal.convolve(spot, kernel, mode='same')
     return sspot, kernels
 
+def shiftSpotSpline(spot, dx, dy, kernels=None, kargs=None):
+    """ shift an image using the scipy ndimage interpolation routines. """
+    
+    if not kargs:
+        kargs = {}
+
+    kernels = []
+    sspot = scipy.ndimage.interpolation.shift(spot, (dy,dx), **kargs)
+    return sspot, kernels
+    
     
 def lanczosWindow(x, n):
     if n > 0: 
@@ -96,7 +106,7 @@ def lanczos3(x):
 def lanczos4(x):
     return lanczosWindow(x, 4)
     
-def sincKernel(offset=0.0, n=3, padding=0, window=lanczos3, doNorm=True):
+def sincKernel(offset=0.0, padding=0, window=lanczosWindow, n=3, doNorm=True):
     if offset < -1 or offset > 1:
         raise ValueError("sinc offset must be in [-1,1], not %0.4f" % (offset))
         
@@ -104,15 +114,15 @@ def sincKernel(offset=0.0, n=3, padding=0, window=lanczos3, doNorm=True):
     left = offset - n - padding
     right = offset + n + padding
 
-    print("n,offset=%d,%0.2f %d %0.2f %0.2f" % (n, offset,
-                                                cnt,
-                                                left, right))
+    #print("n,offset=%d,%0.2f %d %0.2f %0.2f" % (n, offset,
+    #                                            cnt,
+    #                                            left, right))
         
     x = numpy.linspace(left, right, cnt)
     y = numpy.sinc(x)
     
-    if window:
-        w = window(x)
+    if window and n>0:
+        w = window(x, n=n)
         y *= w
     
     if doNorm:
@@ -123,7 +133,7 @@ def sincKernel(offset=0.0, n=3, padding=0, window=lanczos3, doNorm=True):
 def make1dKernel(n=3, offset=0.0, padding=0, doNorm=True):
     """ Construct a centered 1-d lanczos-windowed sinc kernel. """
 
-    x, y = sincKernel(n=n, offset=offset, padding=padding)
+    x, y = sincKernel(n=n, offset=-offset, padding=padding)
     
     if doNorm:
         y /= numpy.sum(y)
@@ -134,7 +144,7 @@ def padArray(arr, padTo):
     assert arr.shape[0] == arr.shape[1]
     assert arr.shape[0] < padTo
 
-    print "old size=%d, new=%d" % (arr.shape[0], padTo)
+    #print "old size=%d, new=%d" % (arr.shape[0], padTo)
     newSize = numpy.array([padTo, padTo])
     offset = (padTo-arr.shape[0])/2
     parr = numpy.zeros(newSize, dtype=arr.dtype)
@@ -146,17 +156,32 @@ def padArray(arr, padTo):
 
 def unpadArray(arr, slice):
     return arr[slice, slice]
+
+def applyPixelResponse(arr, pixelSize):
+    kernelSize = pixelSize * 3
+    kern = numpy.zeros((kernelSize, kernelSize), dtype=arr.dtype)
+    kern[pixelSize:2*pixelSize, pixelSize:2*pixelSize] = 1
+    kern /= numpy.sum(kern)
     
-def poo(arr, dx, dy, binFactor=10, padTo=0, kargs=None):
+    out = scipy.signal.fftconvolve(arr, kern, mode='same')
+
+    return out
+    
+def poo(arr, dx, dy, binFactor=10, padTo=0, applyPixelResp=False, kargs=None):
     assert dx>=0 and dy>=0
-    
+
     # Trim raw image to multiples of binFactor pixels.
     maxSize = binFactor*(numpy.array(arr.shape,dtype='i4')/binFactor)
     newSize = (maxSize / binFactor).tolist()
     arr = arr[:maxSize[0],:maxSize[1]].copy()
 
     # Get our unshifted, binned, reference image.
-    arr00 = pfs_tools.rebin(arr, *newSize)
+    if applyPixelResp:
+        arrs = applyPixelResponse(arr, 10)
+        arr00 = pfs_tools.rebin(arrs, *newSize)
+    else:
+        arr00 = pfs_tools.rebin(arr, *newSize)
+
     if padTo:
         arr00unpadded = arr00.copy()
         arr00, padSlice = padArray(arr00, padTo)
@@ -164,13 +189,16 @@ def poo(arr, dx, dy, binFactor=10, padTo=0, kargs=None):
     # Put the rest of this in loop....
     
     # interpolation-shift the binned reference image
-    arrShifted, kernels = shiftSpot2d(arr00, float(dx)/binFactor, float(dy)/binFactor, kargs=kargs)
+    arrShifted, kernels = shiftSpot(arr00, float(dx)/binFactor, float(dy)/binFactor, kargs=kargs)
 
-    # And pixel-shift then pixel-bin a comparison image
+    # And pixel-shift (and optionally apply pixel response) then pixel-bin a comparison image
     arrPlaced = arr * 0
     arrPlaced[dy:,dx:] = arr[slice(None,-dy if dy else None),
                              slice(None,-dx if dx else None)]
+    if applyPixelResp:
+        arrPlaced = applyPixelResponse(arrPlaced, 10)
     arrPlaced = pfs_tools.rebin(arrPlaced, *newSize)
+
     if padTo:
         arrShifted = unpadArray(arrShifted, padSlice)
         arr00up = unpadArray(arr00, padSlice)
@@ -186,28 +214,31 @@ def dispSpot(spotDict, key):
 
     f = plt.figure()
     
-def gatherPoo(spot, kargs=None):
-    tries = ([1,0,(0,)],
-             [2,0,(0,1)],
-             [3,0,(0,1,2)],
-             [4,128,(0,1,2)],
-             [4,0,(0,1,2)],
-             [5,128,(0,1,2,3)],
-             [6,128,(0,1,2,3)],
-             [7,128,(0,1,2,3,4)],
-             [8,128,(0,1,2,3,4,5)],
-             [8,0,(0,1,2,3,4,5)],
-             [9,128,(0,1,2,3,4,8)],
-             [10,128,(0,1,2,3,4,5,6,9)],
-             [10,0,(0,1,2,3,4,5,6,9)])
+def gatherPoo(spot, applyPixelResp=False, kargs=None):
+    tries = ([1,0],
+             [2,0],
+             [3,0],
+             [4,0],
+             [4,128],
+             [5,0],
+             [6,0],
+             [7,0],
+             [8,0],
+             [8,128],
+             [9,0],
+             [10,128],
+             [10,0])
 
     if not kargs:
-        kargs = dict(n=3, padding=2)
+        kargs = dict(n=3)
         
     all = {}
-    for bin,pad,shifts in tries:
-        for shift in shifts:
-            ret = poo(spot, shift, 0, binFactor=bin, 
+    for bin,pad in tries:
+        for shift in range(bin):
+            print "processing %s with kargs=%s" % ((bin,pad,shift), kargs)
+            ret = poo(spot, shift, 0, 
+                      applyPixelResp=applyPixelResp,
+                      binFactor=bin, 
                       padTo=pad,
                       kargs=kargs)
             freq = imfreq1d(ret[2])
