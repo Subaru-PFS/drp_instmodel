434887875c6c9ea113cfd4b02f1bacc313771f96
Craig Loomis <cloomis@astro.princeton.edu>
2013-04-05 10:46:22 -0400

    Believe that JEG's current format is getting better:
     - do not normalize spots fluxes
     - do not rebin
     - read spot directly from .imgstk file
     - use current filename template to find dataset: ../$date/$band/"*.dat_foc%d_frd%d.imgstk" % (focus, frd)

--- python/pfs_instmodel/jegSpots.py
+++ python/pfs_instmodel/jegSpots.py
@@ -1,4 +1,5 @@
 import glob
+import gzip
 import os
 import re
 import sys
@@ -9,13 +10,11 @@ import scipy.ndimage
 
 import pyfits
 
-import splinedPsf
 import pfs_tools
 
 from pfs_tools import pydebug
 """
-Read the raw PSF spot images from JEG's optical design, and convert
-them to some slightly more efficient intermediate FITS file..
+Read the raw PSF spot images from JEG's optical design. 
 
 The raw file 
 OFFSETS :
@@ -25,6 +24,26 @@ OFFSETS :
     DATA (NIMAGE*XSIZE*YSIZE u-shorts) 32K
 
 """
+
+def getDataPath(date, band, frd, focus=0, spotDir=None):
+    """ Given """
+    if not spotDir:
+        spotDir = os.path.join(os.environ['PFS_INSTDATA_DIR'], 'data/spots/jeg')
+        
+    spotFile = os.path.join(spotDir, date, band, 
+                            "*.dat_foc%d_frd%d.imgstk" % (focus, frd))
+
+    files = glob.glob(spotFile) + glob.glob(spotFile + '.gz')
+    if len(files) != 1:
+        raise RuntimeError("There is not a unique JEG spotfile matching %s{.gz}; found %d" % 
+                           (spotFile, len(files)))
+
+    return files[0]
+
+def resolveSpotPathSpec(pathSpec):
+    if isinstance(pathSpec, basestring):
+        return pathSpec
+    return getDataPath(**pathSpec)
 
 def displaySpot(spot, scale=15.0):
     import xplot
@@ -64,15 +83,21 @@ def getFiberIds(header, headerVersion):
                         
     angs = numpy.array(angs)
     normAngs = angs / angs.max()
-    fibers = (normAngs * 300).astype('i2')
+    fibers = (normAngs * 314).astype('i2')
 
     print "fiber angles: %s" % (angs)
     print "fiber IDs   : %s" % (fibers)
     
     return fibers
-    
-def readSpotDir(path, doConvolve=None, filePattern='*.imgstk'):
-    """ Read slightly cleaned up versions of JEGs spots.
+
+_spotCache = dict()
+def clearSpotCache():
+    global _spotCache
+    _spotCache.clear()
+
+def readSpotFile(pathSpec, doConvolve=None, doRebin=False, doNorm=False, 
+                 verbose=False, clearCache=False):
+    """ Directly read some recent version of JEGs spots.
 
     The .imgstk file contains a few 1k-aligned sections:
     
@@ -84,15 +109,19 @@ def readSpotDir(path, doConvolve=None, filePattern='*.imgstk'):
         
     """
 
-    names = glob.glob(os.path.join(path, '*.imgstk'))
-    assert len(names) == 1, "path %s does not have exactly one .imgstk file" % (path)
-    name = names[0]
-    if name.endswith('.gz'):
+    path = resolveSpotPathSpec(pathSpec)
+    if clearCache:
+        clearSpotCache()
+
+    if path in _spotCache:
+        return _spotCache[path]
+    
+    if path.endswith('.gz'):
         fopen = gzip.open
     else:
         fopen = open
 
-    with fopen(names[0], 'r') as f:
+    with fopen(path, 'r') as f:
         rawHeader = f.read(2*1024)
         rawDesign = f.read((8-2)*1024)    # unused, so far.
         rawPositions = f.read((32-8)*1024)
@@ -147,19 +176,22 @@ def readSpotDir(path, doConvolve=None, filePattern='*.imgstk'):
         else:
             spot = data[i,:,:]
 
-        # bin from 256x256 1um pixels to 85x85 3um pixels.
-        # Still oversampled by 5.
-        spot = spot[:-1,:-1]
-        spot = pfs_tools.rebin(spot, 85,85)
+        if doRebin:
+            # bin from 256x256 1um pixels to 85x85 3um pixels.
+            # Still oversampled by 5.
+            spot = spot[:-1,:-1]
+            spot = pfs_tools.rebin(spot, 85,85)
 
-        # Require total flux to be 1.0
-        spot /= spot.sum()
+        if doNorm:
+            # Require total flux to be 1.0
+            spot /= spot.sum()
         
         # Rotate x-up mechanical view to y-up detector view (dispersing along columns)
         spot = numpy.swapaxes(spot,0,1)
         xc, yc = yc, xc
         spots.append((fiberIdx, wavelength, xc, yc, spot))
-        print("spot  %d (%d, %0.2f) at (%0.1f %0.1f)" % (i, fiberIdx, wavelength, xc, yc))
+        if verbose:
+            print("spot  %d (%d, %0.2f) at (%0.1f %0.1f)" % (i, fiberIdx, wavelength, xc, yc))
         if fiberIdx != 0:
             symSpots.append((-fiberIdx, wavelength, -xc, yc, spot[::-1,:]))
 
@@ -174,6 +206,8 @@ def readSpotDir(path, doConvolve=None, filePattern='*.imgstk'):
 
     # Now clean up... later steps expect to have fiber IDs and wavelengths in order
     arr = numpy.sort(tarr, order=('fiberIdx','wavelength'))
+
+    _spotCache[path] = arr
 
     return arr
 
