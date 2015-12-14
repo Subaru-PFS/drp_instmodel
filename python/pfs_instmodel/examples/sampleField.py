bc4b9cfab7bea0722cbe6857e4ba0ada2ed90c13
Craig Loomis <cloomis@astro.princeton.edu>
2015-12-14 16:19:31 -0500

    Add support for PFS slit geometry an LAM slits,

--- python/pfs_instmodel/examples/sampleField.py
+++ python/pfs_instmodel/examples/sampleField.py
@@ -34,6 +34,84 @@ def keepEven(input):
         if i%2 == 0:
             yield i
 
+class Fiber(object):
+    SCIENCE = 1
+    ENGINEERING = 2
+    BLANK = 3
+
+class Slit(object):
+    block1 = ([Fiber.ENGINEERING] +
+              42 * [Fiber.SCIENCE] +
+              [Fiber.BLANK])
+    block2 = ([Fiber.ENGINEERING] +
+              45 * [Fiber.SCIENCE] +
+              [Fiber.BLANK])
+    block3 = ([Fiber.ENGINEERING] +
+              [Fiber.BLANK] +
+              42 * [Fiber.SCIENCE] +
+              [Fiber.BLANK])
+    block4 = ([Fiber.ENGINEERING] +
+              42 * [Fiber.SCIENCE] +
+              [Fiber.ENGINEERING])
+    block5 = ([Fiber.ENGINEERING] +
+              45 * [Fiber.SCIENCE] +
+              [Fiber.ENGINEERING])
+    gap = 19*[Fiber.BLANK]
+
+    slit1 = (block1 +
+             block2 +
+             block3 +
+             block2 +
+             block3 +
+             block1 +
+             block4 +
+             gap +
+             block5 +
+             block1[::-1] +
+             block3[::-1] +
+             block1[::-1] +
+             block3[::-1] +
+             block2[::-1] +
+             block1[::-1])
+
+    def __init__(self):
+        self.fiber0 = -325
+        self.scienceFibers = []
+        self.engineeringFibers = []
+
+        for i, fiberType in enumerate(self.slit1):
+            if fiberType is Fiber.SCIENCE:
+                self.scienceFibers.append(i)
+            elif fiberType is Fiber.ENGINEERING:
+                self.engineeringFibers.append(i)
+                
+    def scienceFiberToSlitPos(self, scienceFiberNum):
+        """ Return the slit position for the given science fiber """
+
+        if scienceFiberNum < 0:
+            indexNum = scienceFiberNum + 300
+        else:
+            indexNum = scienceFiberNum + 299
+            
+        return self.fiber0 + self.scienceFibers[indexNum]
+
+slit1 = Slit()
+
+# Science fiber numbers, -300 to +300:
+LamSlit1Fibers = [-300, -240, -100, -30, -1, 1, 30, 100, 170, 240, 300]
+LamSlit2Fibers = [-299, -290, -246, -194, -148, -112, -56, -10 -2,
+                   2, 10, 56, 112, 148, 194, 148, 194, 246, 290]
+LamSlit1 = [slit1.scienceFiberToSlitPos(f) for f in LamSlit1Fibers]
+LamSlit2 = [slit1.scienceFiberToSlitPos(f) for f in LamSlit2Fibers]
+
+Lam1Flat = [PROBE(i,0.0,1.0,100.0,200.0,'SIMFLAT', ()) for i in LamSlit1]
+Lam1Arcs = [PROBE(i,0.0,1.0,100.0,200.0,'SIMARC', ()) for i in LamSlit1]
+Lam1CdArcs = [PROBE(i,0.0,1.0,100.0,200.0,'SIMARC', ('CdI',)) for i in LamSlit1]
+Lam1HgArcs = [PROBE(i,0.0,1.0,100.0,200.0,'SIMARC', ('HgI',)) for i in LamSlit1]
+Lam1NeArcs = [PROBE(i,0.0,1.0,100.0,200.0,'SIMARC', ('NeI',)) for i in LamSlit1]
+Lam1XeArcs = [PROBE(i,0.0,1.0,100.0,200.0,'SIMARC', ('XeI',)) for i in LamSlit1]
+Lam1KrArcs = [PROBE(i,0.0,1.0,100.0,200.0,'SIMARC', ('KrI',)) for i in LamSlit1]
+
 bundledField = [i for i in keepInBundle(halfField())]
 centerRange = [i for i in keepInBundle(range(-3,2))]
 edgeRange = [i for i in keepInBundle(range(-fiberLim, -fiberLim+4))]
